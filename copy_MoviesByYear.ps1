eec14d39d872f40b7fb402f03eab8bdd63a25768
Zack T <MLBZ521@gmail.com>
2017-08-15 00:51:35 -0700

    v1.0 = Initial Version
    Simple script I wrote to copy movies from one destination to another.  Mainly for use when copying to an external media and I only want recent movies.

---
+++ copy_MoviesByYear.ps1
@@ -0,0 +1,34 @@
+﻿<#
+
+Script Name:  copy_MoviesByYear.ps1
+By:  Zack Thompson / Created:  8/5/2017
+Version: 1.0 / Updated:  8/5/2017 / By:  ZT
+
+Description:  This script will allow for batch copying of files based on the Year in the title.
+
+#>
+
+# ============================================================
+# Define Variables
+# ============================================================
+$Source = "M:\Library\Kids"
+$Destination = "H:\Movies"
+$Position=1
+
+# ============================================================
+# Script Body
+# ============================================================
+$filesToMove = Get-ChildItem $Source | Where { $_.Name -match "2014" }
+$totalFiles=$filesToMove.count
+
+Write-Host "There are $totalFiles matching the criteria."
+
+ForEach ( $File in $filesToMove ) {
+    If (!(Test-Path -Path "$Destination\$File")){
+        Write-Host "$File does not exist on the destintation drive.  Copying now..."
+        Write-Host "File $Position of $totalFiles."
+        Write-Progress -Activity "Copying data from $Source to $Destination" -Status "Copying File $File" -PercentComplete (($Position/$totalFiles)*100)
+        Copy-Item $File.FullName $Destination -Recurse
+        $Position++
+    }
+}
